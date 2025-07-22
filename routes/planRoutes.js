71c85427278f84e83670ffa715785f705e4248e2
Libity13 <rock80537@gmail.com>
2025-07-22 20:19:09 +0700

    Initial commit with updated Tripadvisor API headers and fixes planRoutes

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -3,9 +3,5 @@ import { createPlan } from "../controllers/planController.js";
 const router = express.Router();
 
 router.post("/", createPlan);
-<<<<<<< HEAD
  
-=======
-
->>>>>>> 073e983d9bfc5de307650dbfb427581aeed9eb41
 export default router; 
